3c252337cf046e58266d2c01c4c4d6daa851bb21
Kehula <kehula1@gmail.com>
2020-07-15 11:28:46 +0300

    simple cache realisation

--- main.go
+++ main.go
@@ -1,5 +1,43 @@
 package main
 
+import (
+	"io/ioutil"
+	"net/http"
+)
+
+type Memo struct {
+	f     Func
+	cache map[string]Result
+}
+
+type Func func(key string) (interface{}, error)
+type Result struct {
+	value interface{}
+	err   error
+}
+
+func New(f Func) *Memo {
+	return &Memo{f: f, cache: make(map[string]Result)}
+}
+
+func (memo *Memo) Get(key string) (interface{}, error) {
+	res, ok := memo.cache[key]
+	if !ok {
+		res.value, res.err = memo.f(key)
+		memo.cache[key] = res
+	}
+	return res.value, res.err
+}
+
 func main() {
 
+}
+
+func httpGetBody(url string) (interface{}, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	return ioutil.ReadAll(resp.Body)
 }
